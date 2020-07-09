9c7f1f3a3f2e47194690bb01328907f25feedd3d
John Baldwin <jhb@FreeBSD.org>
2020-07-08 18:28:42 -0700

    Add a cheri_init_capabilities() and call it from locore.
    This still doesn't save the omnipotent cap anywhere, but now we can do
    it in C when the time comes.  While here, constrain the userspace_cap
    a bit (copied from MIPS).  This is also true for hybrid, not just
    purecap.

--- sys/riscv/include/cheri.h
+++ sys/riscv/include/cheri.h
@@ -41,6 +41,7 @@
 /*
  * CHERI-RISC-V-specific kernel utility functions.
  */
+void	cheri_init_capabilities(void * __capability kroot);
 int	cheri_sccsr_to_sicode(register_t sccsr);
 void	hybridabi_thread_setregs(struct thread *td, unsigned long entry_addr);
 #endif
